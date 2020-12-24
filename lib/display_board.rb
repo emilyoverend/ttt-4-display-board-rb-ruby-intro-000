07cc547a20e64701efc85d101ddc0e0b8e26ea57
Emily Overend <eo17233@gmail.com>
2020-12-24 02:21:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -42,5 +42,4 @@ end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
 
-board = [" ", " ", " ", "X", " ", " ", " ", " ", " "]
-display_board(board)
+display_board(board[4] = "X")
